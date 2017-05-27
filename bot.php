02d75b841b219f2f36b86df290f07a2ecd7e4a77
Xecond <panukom.ko@gmail.com>
2017-05-27 15:39:55 +0700

    Replay as image test

--- bot.php
+++ bot.php
@@ -17,16 +17,14 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+			if(strpos($text, 'แมว') != false)
+
+			$data = ['imageFile' => 'http://siberiantimes.com/upload/information_system_52/1/8/4/item_1849/information_items_1849.jpg',]
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
